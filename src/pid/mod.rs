f67aa7a5f07fdf9cd8e9545ee7e3884a29d33148
Arnaud Lefebvre <a.lefebvre@outlook.fr>
2017-02-18 17:30:22 +0100

    Add parser for /proc/<pid>/limits

--- src/pid/mod.rs
+++ src/pid/mod.rs
@@ -1,12 +1,14 @@
 //! Process-specific information from `/proc/[pid]/`.
 
 mod cwd;
+mod limits;
 mod mountinfo;
 mod stat;
 mod statm;
 mod status;
 
 pub use pid::cwd::{cwd, cwd_self};
+pub use pid::limits::{Limits, limits, limits_self};
 pub use pid::mountinfo::{Mountinfo, mountinfo, mountinfo_self};
 pub use pid::statm::{Statm, statm, statm_self};
 pub use pid::status::{SeccompMode, Status, status, status_self};
