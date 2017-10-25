ef6828c9511a0e453496917a9f718c217cc5c083
Andy Lowry <andy@andylowry.me.uk>
2017-10-25 16:13:25 +0100

    Fix for 64 bit android

--- src/pid/status.rs
+++ src/pid/status.rs
@@ -187,9 +187,9 @@ named!(parse_status_state<State>,
           | tag!("Z (zombie)") => { |_| State::Zombie }));
 
 named!(parse_command<String>,   delimited!(tag!("Name:\t"),      parse_line,         line_ending));
-#[cfg(target_os = "android")]
+#[cfg(all(target_os = "android", not(target_arch = "aarch64")))]
 named!(parse_umask<mode_t>,     delimited!(tag!("Umask:\t"),     parse_u16_octal,    line_ending));
-#[cfg(not(target_os = "android"))]
+#[cfg(not(all(target_os = "android", not(target_arch = "aarch64"))))]
 named!(parse_umask<mode_t>,     delimited!(tag!("Umask:\t"),     parse_u32_octal,    line_ending));
 named!(parse_state<State>,      delimited!(tag!("State:\t"),     parse_status_state, line_ending));
 named!(parse_pid<pid_t>,        delimited!(tag!("Tgid:\t"),      parse_i32,          line_ending));
