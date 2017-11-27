e612acfd720d0b2ae12dc190e008c648c6419a93
Andy Lowry <andy@andylowry.me.uk>
2017-11-27 10:20:01 +0000

    Fix for x86 android

--- src/pid/status.rs
+++ src/pid/status.rs
@@ -187,9 +187,9 @@ named!(parse_status_state<State>,
           | tag!("Z (zombie)") => { |_| State::Zombie }));
 
 named!(parse_command<String>,   delimited!(tag!("Name:\t"),      parse_line,         line_ending));
-#[cfg(all(target_os = "android", not(target_arch = "aarch64")))]
+#[cfg(all(target_os = "android", target_arch = "arm"))]
 named!(parse_umask<mode_t>,     delimited!(tag!("Umask:\t"),     parse_u16_octal,    line_ending));
-#[cfg(not(all(target_os = "android", not(target_arch = "aarch64"))))]
+#[cfg(not(all(target_os = "android", target_arch = "arm")))]
 named!(parse_umask<mode_t>,     delimited!(tag!("Umask:\t"),     parse_u32_octal,    line_ending));
 named!(parse_state<State>,      delimited!(tag!("State:\t"),     parse_status_state, line_ending));
 named!(parse_pid<pid_t>,        delimited!(tag!("Tgid:\t"),      parse_i32,          line_ending));
